d349bd171b3d04239af653b44ee278f769674ce0
Халилов Айбулат-kaien <m_halilovaybulat@mail.ru>
2023-09-19 04:05:00 +0500

    .

---
+++ Lesson-02/task_02-01_complete.py
@@ -0,0 +1,11 @@
+print("Введите названия городов")
+city1 = input()
+
+if city1 == "Тула":
+    city2 = input()
+    if city2 == "Нижний новгород":
+        print("ДА")
+    else:
+        print("НЕТ")
+else:
+    print("НЕТ")
