a2d664cac9e10f17b89f2332e40ad6d8877cc1ec
Халилов Айбулат-kaien <m_halilovaybulat@mail.ru>
2023-09-19 09:06:26 +0500

    03-06 complete

---
+++ Lesson-03/task_03-06.py
@@ -0,0 +1,16 @@
+print("--Проверка паролей--")
+
+answer = ""
+while answer != "OK":
+    password1 = input("Пароль: ")
+    password2 = input("Подтвердите пароль: ")
+    if len(password1) < 8:
+        print("Короткий!")
+    elif "123" in password1:
+        print("Простой!")
+    elif password1 != password2:
+        print("Не совпадают")
+    else:
+        answer = "OK"
+        print(answer)
+
